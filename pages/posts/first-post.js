7674f626778bfc4855afb66b51614c3a08769279
joshua atanu <atanu.joshua@gmail.com>
2023-04-02 12:29:16 +0100

    created the blog post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
+import Script from 'next/script'
+import Layout from '../../components/Layout'
+
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+
+      <Head>
+        <title>First post</title>
+        {/* <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() => {
+            console.log(`script loaded correctly , window.FB has been populated`)
+          }}
+        ></Script> */}
+      </Head>
       <h1>First post</h1>
-      <h2> <Link href='/'> Back to Home</Link></h2>
-    </>
+      <h2>
+        <Link href='/'> Back to Home</Link>
+      </h2>
+    </Layout>
 
   )
 }
